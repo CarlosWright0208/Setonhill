7bf281b3a0e4ddae0041bb52c81f4cebe364c3ae
carloswright <carloswright@10.242.1.151>
2019-09-12 14:05:40 -0400

    ready

---
+++ HW1/src/hw1.java
@@ -0,0 +1,23 @@
+
+public class hw1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello this is line 1");
+		System.out.println("Goodbye this is line 2");
+		System.out.println("talk to you this is line 3");
+		System.out.println("how are you this is line 4");
+		System.out.println("see you later this is line 5");
+		System.out.println("hows it going this is line 6");
+		System.out.println("good to meet you this is line 7");
+		System.out.println("good luck this is line 8");
+		System.out.println("how you been this is line 9");
+		System.out.println("good afternoon this is line 10");
+		
+		
+		
+	}
+
+}
