c15ceb553e84b39d3fc437721fc9c86348200f11
carloswright <carloswright@10.242.1.151>
2019-09-23 13:17:27 -0400

    ready

---
+++ wright program 1/src/profram1.java
@@ -0,0 +1,17 @@
+
+public class profram1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int age=18;
+char middle='d';
+double speed=4;
+System.out.println("my age is "+age);
+System.out.println("my middle is "+middle);
+System.out.println("my speed is "+speed);
+
+	
+	}
+
+}
